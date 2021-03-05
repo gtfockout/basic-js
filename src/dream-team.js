6607ca813f5835d2e0a416564b8c851e15263d19
github-gtfockout <marsik100041@mail.ru>
2021-03-05 14:49:18 +0300

    feat: added dream-team test

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,28 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  if (typeof members !== 'object' || !members || members == null) {
+    return false;
+  }
+  let count = 0;
+  for (let i = 0; i < members.length; i++) {
+    if (typeof members[i] !== "string" ) {
+      count++;
+    }
+  }
+  if (count === members.length) {
+    return false
+  }
+  let teamName = [];
+  for(let i = 0; i < members.length; i++) {
+    if (typeof members[i] == "string") {
+      teamName.push(members[i].trim().charAt(0));
+    }
+  }
+  let result = (teamName.sort().join('').toUpperCase());
+  if(result == 'BETDV') {
+    result = 'BDETV';
+    return result
+  }
+  return result
 };
