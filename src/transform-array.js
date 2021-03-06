eb507a66e7a613eed7828f6740f77a188d8c1323
github-gtfockout <marsik100041@mail.ru>
2021-03-06 12:55:19 +0300

    feat: added transform-array task

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,46 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(arr) {
+  let newArr = [];
+  let discardN = "--discard-next";
+  let discardP = "--discard-prev";
+  let doubleN = "--double-next";
+  let doubleP = "--double-prev";
+  if (arr instanceof Array) {
+    for (let i = 0; i < arr.length; i++) {
+      if (i === 0) {
+        if (arr[i] === discardP || arr[i] === doubleP) {
+          continue;
+        }
+      }
+      if (i === arr.length - 1) {
+        if (arr[i] === doubleN || arr[i] === discardN) {
+          continue;
+        }
+      }
+      if (arr[i] === discardN) {
+        i++;
+      }
+      else if (arr[i] === discardP) {
+        if (!(arr[i - 2] === discardN)) {
+          newArr.pop();
+        }
+      }
+      else if (arr[i] === doubleN) {
+        newArr.push(arr[i + 1]);
+      }
+      else if (arr[i] === doubleP) {
+        if (!(arr[i - 2] === discardN)) {
+          newArr.push(arr[i - 1]);
+        }
+      }
+      else {
+        newArr.push(arr[i]);
+      }
+    }
+  }
+  else {
+    throw new Error()
+  }
+  return newArr
 };
