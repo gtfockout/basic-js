ef1b0a6821fe9d32f0fb2a24859d0a1db492091a
github-gtfockout <marsik100041@mail.ru>
2021-03-09 17:41:24 +0300

    feat: added extended-repeater task

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,35 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+  if (typeof(options.separator) === 'undefined') {
+    options.separator = '+';
+  } 
+	if (typeof(options.additionSeparator) === 'undefined') {
+    options.additionSeparator = '|';
+  } 
+	let result = '';
+  if (typeof(options.addition) !== 'undefined') {
+    let i = 0;
+    do {
+      if (i !== options.additionRepeatTimes-1 && typeof(options.additionRepeatTimes) === 'number') {
+        str += `${options.addition}${options.additionSeparator}`;
+      }
+      else {
+        str += `${options.addition}`;
+      }
+      i++;
+    } while (i < options.additionRepeatTimes)
+  }
+	let j = 0;
+	do {
+		if (j !== options.repeatTimes-1 && typeof(options.repeatTimes) === 'number') {
+      result += `${str}${options.separator}`;
+    }
+		else {
+      result += `${str}`;
+    }
+		j++;
+	} while (j < options.repeatTimes)
+	return result
 };
   
